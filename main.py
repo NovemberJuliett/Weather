078d1ffb2a2e2f23b95f4a9ef7657f05dbee747c
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-08-30 17:36:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import requests
 
-url = 'https://wttr.in/san%20francisco?nTqu&lang=en%20HTTP/1.1'
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
-
 url_template = "https://wttr.in/{}"
-response1 = requests.get(url_template.format("svo"))
-response2 = requests.get(url_template.format("cherepovets"))
-response3 = requests.get(url_template.format("london"))
-print(response1.text, response2.text, response3.text)
+response_svo = requests.get(url_template.format("svo"))
+response_london = requests.get(url_template.format("london"))
+print(response_svo.text, response_london.text)
 
 url_ru = "https://wttr.in/Череповец?lang=ru&?q&?T&?n&?M&?m"
-response4 = requests.get(url_ru)
-print(response4.text)
+response_new_cherepovets = requests.get(url_ru)
+print(response_new_cherepovets.text)
