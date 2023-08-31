d47f414b36522999e1400c6d3f06b7e1f64df81a
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-08-31 13:47:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ response_svo = requests.get(url_template.format("svo"))
 response_london = requests.get(url_template.format("london"))
 print(response_svo.text, response_london.text)
 
+
 url_ru = "https://wttr.in/Череповец?lang=ru&?q&?T&?n&?M&?m"
 response_new_cherepovets = requests.get(url_ru)
 print(response_new_cherepovets.text)
