a659e15e78914fcbd068e49b416e7f47ea707108
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-09-01 16:58:56 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
 import requests
-import logging
-
-cities_list = [
-    "SVO",
-    "London",
-    "Череповец"
-]
 
 
-def request_weather():
+def main():
+    cities_list = [
+        "SVO",
+        "London",
+        "Череповец"
+    ]
+
     for i in cities_list:
         url_template = "https://wttr.in/{}"
         response = requests.get(url_template.format(i))
         response.raise_for_status()
-        return response.text
+        print(response.text)
 
-
-def request_otherweather():
-    for i in cities_list:
-        url_template = "http://wttr.dvmn.org//{}"
-        response = requests.get(url_template.format(i))
-        response.raise_for_status()
-        return response.text
-
-
-try:
-    forecast = request_weather()
-except requests.exceptions.ConnectionError:
-    forecast = request_otherweather()
-
-
-cherepovets_param = {"?lang=": "ru", "?M": "", "?m": "", "?n": "", "?q": "", "T": ""}
-response_new_cherepovets = requests.get("https://wttr.in/Череповец", params=cherepovets_param)
-response_new_cherepovets.raise_for_status()
-print(response_new_cherepovets.url)
+    cherepovets_param = \
+        {"?lang=": "ru", "?M": "", "?m": "", "?n": "", "?q": "", "T": ""}
+    response_new_cherepovets = \
+        requests.get("https://wttr.in/Череповец", params=cherepovets_param)
+    response_new_cherepovets.raise_for_status()
+    print(response_new_cherepovets.url)
 
 
 
-
-
+if __name__ == '__main__':
+    main()
